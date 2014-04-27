bc40e7db9a4c879d95875225459d137d1ae1c64e
vincent <vincent.lefoulon@free.fr>
2014-04-27 22:40:11 +0200

    Finish the functions

--- TP04/tp04.ml
+++ TP04/tp04.ml
@@ -1,8 +1,3 @@
-(*
-	LEFOULON Vincent
-	Ocaml
-*)
-
 (* 'a array -> 'a *)
 let min_array a =
   let m = ref a.(0) in
@@ -23,73 +18,98 @@ let int_of_bool b =
 (* 1. *)
 (* 'a array -> 'a array -> int *)
 let distance_naive x y =
-	let rec aux i j = (* Renvoie dl(xi, yj) *)
-		match i, j with
-			| 0, _ -> j
-		  | _, 0 -> i
-			| _, _ -> let cij = int_of_bool (x.(i-1) != y.(j-1)) in
-									min_array [|
-										aux (i-1) j + 1;
-										aux i (j-1) + 1;
-										aux (i-1) (j-1) + cij 
-									|]
-	in
-	aux (Array.length x) (Array.length y)
+  let rec aux i j = (* Renvoie dl(xi, yj) *)
+    match i, j with
+      | 0, _ -> j
+      | _, 0 -> i
+      | _, _ -> let cij = int_of_bool (x.(i-1) != y.(j-1)) in
+                  min_array [|
+                    aux (i-1) j + 1;
+                    aux i (j-1) + 1;
+                    aux (i-1) (j-1) + cij 
+                  |]
+  in
+  aux (Array.length x) (Array.length y)
 ;;
 
 (* 2. *)
 let distance_memo x y =
-	let xLen = Array.length x in
-	let yLen = Array.length y in
-	let tbl = Hashtbl.create ((xLen+1) * (yLen+1)) in
-	let memo_aux i j =
-		try
-			Hashtbl.find tbl (i, j)
-		with
-			| Not_found -> begin
-											let a = aux i j in
-											Hashtbl.add tbl (i, j) a;
-											a
-										 end
-	and rec aux i j = (* Renvoie dl(xi, yj) *)
-		match i, j with
-			| 0, _ -> j
-		  | _, 0 -> i
-			| _, _ -> let cij = int_of_bool (x.(i-1) != y.(j-1)) in
-								
-									min_array [|
-										a + 1;
-										b + 1;
-										c + cij 
-									|]
-	in
-	aux (Array.length x) (Array.length y)
+  let xLen = Array.length x in
+  let yLen = Array.length y in
+  let tbl = Hashtbl.create ((xLen+1) * (yLen+1)) in
+  let rec aux_memo i j =
+    try
+      Hashtbl.find tbl (i, j)
+    with
+      | Not_found -> begin
+                      let a = aux i j in
+                      Hashtbl.add tbl (i, j) a;
+                      a
+                     end
+  and aux i j = (* Renvoie dl(xi, yj) *)
+    match i, j with
+      | 0, _ -> j
+      | _, 0 -> i
+      | _, _ -> let cij = int_of_bool (x.(i-1) != y.(j-1)) in
+                  min_array [|
+                    aux_memo (i-1) j + 1;
+                    aux_memo i (j-1) + 1;
+                    aux_memo (i-1) (j-1) + cij 
+                  |]
+  in
+  aux (Array.length x) (Array.length y)
 ;;
 
 (* 3. *)
+(*
+  On remplit la matrice et retourne l'élément en bas à droite.
+*)
 let distance x y =
-	let xLen = Array.length x in
-	let yLen = Array.length y in
-	let matDefault = -1 in
-	let mat = Array.make_matrix (xLen+1) (yLen+1) matDefault in
-	let rec aux i j = (* Renvoie dl(xi, yj) *)
-		match i, j with
-			| 0, _ -> j
-		  | _, 0 -> i
-			| _, _ -> let cij = int_of_bool (x.(i-1) != y.(j-1)) in
-									if mat.(i-1).(j) = matDefault then mat.(i-1).(j) <- aux (i-1) j;
-									if mat.(i).(j-1) = matDefault then mat.(i).(j-1) <- aux i (j-1);
-									if mat.(i-1).(j-1) = matDefault then mat.(i-1).(j-1) <- aux (i-1) (j-1);
-									min_array [|
-										mat.(i-1).(j) + 1;
-										mat.(i).(j-1) + 1;
-										mat.(i-1).(j-1) + cij 
-									|]
-	in
-	aux xLen yLen
+  let xLen = Array.length x in
+  let yLen = Array.length y in
+  let matDefault = -1 in
+  let mat = Array.make_matrix (xLen+1) (yLen+1) matDefault in
+  for i = 0 to xLen do
+    for j = 0 to yLen do
+      mat.(i).(j) <- if j = 0 then i
+                     else if i = 0 then j
+                     else begin 
+                       let cij = int_of_bool (x.(i-1) != y.(j-1)) in
+                       min_array [|
+                         mat.(i-1).(j) + 1;
+                         mat.(i).(j-1) + 1;
+                         mat.(i-1).(j-1) + cij 
+                       |]
+                     end
+    done;
+  done;
+  mat.(xLen).(yLen)
 ;;
 
 (* 4. *)
+(*
+  Pour obtenir dl(x_{i}, y_{j}), il n'est pas nécessaire de tout connaître,
+  seulement dl(x_{i}, y_{j-1}), dl(x_{i-1}, y_{j}), dl(x_{i-1}, y_{j-1}).
+  Du coup, on ne conserve qu'une matrice (n, 2).
+*)
 let distance_opt x y =
-	"TODO"
+  let xLen = Array.length x in
+  let yLen = Array.length y in
+  let currentCol = Array.make (yLen+1) (-1) in
+  for i = 0 to xLen do
+    let oldCol = Array.copy currentCol in
+    for j = 0 to yLen do (* On remplit la ième colonne. *)
+      currentCol.(j) <- if j = 0 then i
+                         else if i = 0 then j
+                         else begin 
+                           let cij = int_of_bool (x.(i-1) != y.(j-1)) in
+                           min_array [|
+                             oldCol.(j) + 1;
+                             currentCol.(j-1) + 1;
+                             oldCol.(j-1) + cij 
+                           |]
+                         end
+    done;
+  done;
+  currentCol.(yLen)
 ;;
--- TP04/tp04.test.ml
+++ TP04/tp04.test.ml
@@ -1,12 +1,30 @@
 #use "tp04.ml";;
 
+print_string "\n min_array :";;
 let a = [|4;6;2;4;1;8;10|];;
 min_array a;; (* 1 *)
 
+(* 1. *)
+print_string "\n distance_naive :";;
 distance_naive [||] [||];; (* 0 *)
 distance_naive [|1;1;1|] [|1;2;1|];; (* 1 *)
 distance_naive [|1;1;1;2|] [|1;1|];; (* 2 *)
 
+(* 2. *)
+print_string "\n distance_memo :";;
+distance_memo [||] [||];; (* 0 *)
+distance_memo [|1;1;1|] [|1;2;1|];; (* 1 *)
+distance_memo [|1;1;1;2|] [|1;1|];; (* 2 *)
+
+(* 3. *)
+print_string "\n distance :";;
 distance [||] [||];; (* 0 *)
 distance [|1;1;1|] [|1;2;1|];; (* 1 *)
 distance [|1;1;1;2|] [|1;1|];; (* 2 *)
+
+(* 4. *)
+print_string "\n distance_opt :";;
+distance_opt [||] [||];; (* 0 *)
+distance_opt [|1;1;1|] [|1;2;1|];; (* 1 *)
+distance_opt [|1;1;1;2|] [|1;1|];; (* 2 *)
+
