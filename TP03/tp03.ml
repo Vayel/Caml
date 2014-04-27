a2b6b8f30a0fa894cc97ac566350e93c68cfe677
vincent <vincent.lefoulon@free.fr>
2014-04-27 08:47:19 +0200

    Add tests

--- TP03/tp03.ml
+++ TP03/tp03.ml
@@ -15,29 +15,33 @@ let diveucli a b =
   in (q, a - b * q)
 ;;
 
-let cons x l = match x, l with
-  | 0, [] -> []
-  | _ -> x::l
+let cons x l = 
+	match x, l with
+		| 0, [] -> []
+	  | _ -> x::l
 ;;
 
-let rec normalise l c = match l with
-  | [] -> cons c []
-  | x::l' -> let q, r = diveucli (x + c) base in
-               cons r (normalise l' q)
+let rec normalise l c = 
+	match l with
+		| [] -> cons c []
+		| x::l' -> let q, r = diveucli (x + c) base in
+              		cons r (normalise l' q)
 ;;
 
 let naturel t = Naturel (normalise t 0);;
 
 (* II.2. *)
-let nb_chiffres n = match n with
-  | Naturel l -> list_length l
+let nb_chiffres n = 
+	match n with
+		| Naturel l -> list_length l
 ;;
 
 (* II.3. *)
-let rec ieme_elem l i = match i, l with
-  | _, [] -> 0
-  | 0, x::r -> x
-  | _, x::r -> ieme_elem r (i-1)
+let rec ieme_elem l i = 
+	match i, l with
+		| _, [] -> 0
+		| 0, x::r -> x
+		| _, x::r -> ieme_elem r (i-1)
 ;;
 
 let chiffre n i = match n with
@@ -46,75 +50,85 @@ let chiffre n i = match n with
 
 (* III. Opérations élémentaires *)
 (* III.1. *)
-let rec ajoute_aux l1 l2 l = match l1, l2 with
-  | [], [] -> rev l
-  | [], x::r -> ajoute_aux [] r ((x+0)::l)
-  | x::r, [] -> ajoute_aux r [] ((x+0)::l)
-  | x::r, x'::r' -> ajoute_aux r r' ((x+x')::l)
+let rec ajoute_aux l1 l2 l = 
+	match l1, l2 with
+		| r, [] -> (rev l) @ r
+		| [], r -> (rev l) @ r
+		| x1::r1, x2::r2 -> ajoute_aux r1 r2 ((x1+x2)::l)
 ;;
 
-let ajoute n1 n2 = match n1, n2 with
-  | Naturel l1, Naturel l2 -> naturel(ajoute_aux l1 l2 [])
+let ajoute n1 n2 = 
+	match n1, n2 with
+  	| Naturel l1, Naturel l2 -> naturel(ajoute_aux l1 l2 [])
 ;;
 
 (* III.2. *)
-let rec soustrait_aux l1 l2 l = match l1, l2 with
-  | [], [] -> rev l
-  | [], x::r -> failwith "soustrait(): n1 < n2"
-  | x::r, [] -> soustrait_aux r [] ((x-0)::l)
-  | x::r, x'::r' -> soustrait_aux r r' ((x-x')::l)
+let rec soustrait_aux l1 l2 l = 
+	match l1, l2 with
+		| [], [] -> rev l
+		| [], x::r -> failwith "soustrait(): n1 < n2"
+		| x::r, [] -> soustrait_aux r [] ((x-0)::l)
+		| x::r, x'::r' -> soustrait_aux r r' ((x-x')::l)
 ;;
 
-let soustrait n1 n2 = match n1, n2 with
-  | Naturel [], Naturel [] -> Naturel []
-  | Naturel l1, Naturel l2 -> naturel(soustrait_aux l1 l2 [])
+let soustrait n1 n2 = 
+	match n1, n2 with
+		| Naturel [], Naturel [] -> Naturel []
+		| Naturel l1, Naturel l2 -> naturel(soustrait_aux l1 l2 [])
 ;;
 
 (* III.3. *)
-let rec compare_aux l1 l2 = match l1, l2 with
-  | [], [] -> 0
-  | [], x::r -> -1
-  | x::r, [] -> 1
-  | x::r, x'::r' -> let d = (x-x') in
-			                if d = 0 then compare_aux r r'
-                      else if d < 0 then -1
-                      else 1
+let rec compare_aux l1 l2 = 
+	match l1, l2 with
+		| [], [] -> 0
+		| [], x::r -> -1
+		| x::r, [] -> 1
+		| x::r, x'::r' -> let d = (x-x') in
+					              if d = 0 then compare_aux r r'
+		                    else if d < 0 then -1
+		                    else 1
 ;;
 
-let compare p q = match p, q with
-  | Naturel l1, Naturel l2 -> compare_aux (rev l1) (rev l2)
+let compare p q = 
+	match p, q with
+  	| Naturel l1, Naturel l2 -> compare_aux (rev l1) (rev l2)
 ;;
 
 (* III.4. *)
-let rec mulc_aux p n l = match n with
-  | [] -> rev l
-  | x::r -> mulc_aux p r ((x * p)::l)
+let rec mulc_aux p n l = 
+	match n with
+		| [] -> rev l
+		| x::r -> mulc_aux p r ((x * p)::l)
 ;;
 
-let mulc p n = match p, n with
-  | 0, Naturel l -> Naturel []
-  | _, Naturel l -> naturel (mulc_aux p l [])
+let mulc p n = 
+	match p, n with
+		| 0, Naturel l -> Naturel []
+  	| _, Naturel l -> naturel (mulc_aux p l [])
 ;;
 
 (* III.5. *)
-let rec prepend n l p = match p with
-  | 0 -> l
-  | _ -> prepend n (n::l) (p-1)
+let rec prepend x l n = 
+	match n with
+  	| 0 -> l
+ 		| _ -> prepend x (x::l) (n-1)
 ;;
 
-let mulb n p = match n, p with
-  | Naturel [], _ -> Naturel []
-  | _, 0 -> n
-  | Naturel l, _ -> Naturel (prepend 0 l p)
+let mulb n p = 
+	match n, p with
+		| Naturel [], _ -> naturel []
+		| Naturel l, _ -> naturel (prepend 0 l p)
 ;;
 
 (* III.6. *)
-let rec mul_naive_aux l1 l2 l = match l1, l2 with
-  | _ -> Naturel []
+let rec mul_naive_aux l1 l2 l = 
+	match l1, l2 with
+  	| _ -> Naturel []
 ;;
 
-let mul_naive n1 n2 = match n1, n2 with
-  | Naturel l1, Naturel [] -> Naturel []
-  | Naturel [], Naturel l2 -> Naturel []
-  | Naturel l1, Naturel l2 -> mul_naive_aux l1 l2 []
+let mul_naive n1 n2 = 
+	match n1, n2 with
+		| Naturel l1, Naturel [] -> Naturel []
+		| Naturel [], Naturel l2 -> Naturel []
+		| Naturel l1, Naturel l2 -> mul_naive_aux l1 l2 []
 ;;
--- TP03/tp03.test.ml
+++ TP03/tp03.test.ml
@@ -2,21 +2,39 @@ include "tp03.ml";;
 
 (* II. Représentation *)
 (* II.1. *)
+naturel [1;3;15;0;0];; (* [1;3;5;1] *)
 
 (* II.2. *)
+let n = naturel [5;4;20;0;0;33;0;0;0];;
+nb_chiffres n;; (* 7 *)
 
 (* II.3. *)
+let n = naturel [5;4;20;0;0;3;0];;
+chiffre n 3;; (* 2 *)
 
 (* III. Opérations élémentaires *)
 (* III.1. *)
+let n1 = naturel [1;9;8;2;4;6;0;0];;
+let n2 = naturel [3;32];;
+ajoute n1 n2;; (* [4;1;2;3;4;6] *)
 
 (* III.2. *)
+let n1 = naturel [1;9;8;2;4;6;0;0];;
+let n2 = naturel [3;32;1;2;7];;
+soustrait n1 n2;; (* [8;6;4;0;7;5] *)
 
 (* III.3. *)
+compare (naturel [1;2;3]) (naturel [1;2;3;0;0]);; (* 0 *)
+compare (naturel [1;2;3;4]) (naturel [1;2;3;5]);; (* -1 *)
+compare (naturel [5;6;2;7]) (naturel [2;3;5;2]);; (* 1 *)
 
 (* III.4. *)
+let n1 = naturel [1;9;8;2;4;6;0;0];;
+mulc 3 n1;; (* [3;7;6;8;2;9;1] *)
 
 (* III.5. *)
+let n1 = naturel [22];;
+mulb n1 2;; (* [0;0;2;2] *)
 
 (* III.6. *)
 
