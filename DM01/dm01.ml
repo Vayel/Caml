1ec93f7dfc8b52481b47496a8ccaa4bd63d7636d
vincent <vincent.lefoulon@free.fr>
2014-05-04 08:59:27 +0200

    Answer some questions

--- DM01/dm01.ml
+++ DM01/dm01.ml
@@ -171,7 +171,7 @@ let cout_glouton req =
 (* III.1. *)
 (*
   La séquence contiendra n valeurs et chacune peut
-  valoir soit 0 soit 1. Il y a donc 2^n possibilités.
+  valoir soit 0 soit 1. Il y a donc 2^{n} possibilités.
 *)
 
 (* III.2.a. *)
@@ -294,57 +294,64 @@ let force_brute_array req =
 
 (* III.4. *)
 (*
-  Il suffit de supprimer les cas où les 1 remplacent les 0 et inversement.
-  Par exemple, ces deux séquences ont le même coût : [1;0;0;1;0;1;1] et [0;1;1;0;1;0;0].
-  Or [1;0;0;1;0;1;1] = [1;1;1;1;1;1;1] - [0;1;1;0;1;0;0]
-  En notant C(dep) le coût de la séquence de déplacements dep, on a, pour n = 3 :
-  C([1;1;1]) = C([0;0;0])
-  C([1;1;0]) = C([0;0;1])
-  C([1;0;1]) = C([0;1;0])
-  C([1;0;0]) = C([0;1;1])
-  Ainsi, dans enum, il ne faut pas appeler range 0 ((pow 2 n) - 1) mais seulement 
-  range 0 ((pow 2 (n-1)) - 1).
+Il suffit de supprimer les cas où les 1 remplacent les 0 et inversement.  
+Par exemple, ces deux séquences ont le même coût : [1;0;0;1;0;1;1] et [0;1;1;0;1;0;0].  
+Or [1;0;0;1;0;1;1] = [1;1;1;1;1;1;1] - [0;1;1;0;1;0;0]  
+En notant C(dep) le coût de la séquence de déplacements dep, on a, pour n = 3 :  
+C([1;1;1]) = C([0;0;0])  
+C([1;1;0]) = C([0;0;1])  
+C([1;0;1]) = C([0;1;0])  
+C([1;0;0]) = C([0;1;1])  
+Ainsi, dans enum, il ne faut pas appeler range 0 ((pow 2 n) - 1) mais seulement 
+range 0 ((pow 2 (n-1)) - 1).
 *)
 
 (* III.5. *)
 (*
-  force_brute_list :
-  	- Appel à enum() : O(2^{n})
-  	- Calcul du coût de chaque séquence de déplacements : O(n)
-  	- Renvoi le minimum d'une liste : O(n) 
-  	force_brute_list() est donc un O(2^{n}).
-  	
-  	Côté mémoire, enum() crée 2^{n} listes de taille n, n étant la longueur de 
-  	la séquence de requêtes.
-  	Dans costs sont donc stockés 2^{n} nombres.
-  	L'espace mémoire maximal utilisé est alors de n * 2^{n} + 2^{n}.
-  	
-  force_brute_array :
-  	- Calcul de la taille de req : O(n)
-  	- Appel à range() : O(2^{n})
-  	- n appels à binaire() : O(n^{2})
-  	- n appels à cout_array() : O(n)
-  	force_brute_array() est donc un O(2^{n}).
-  	
-  	Côté mémoire, on stocke un nombre dans len, une liste de taille 2^{n} dans r
-  	et un nombre dans m.
-  	L'espace mémoire maximal utilisé est alors de 2^{n}.
+**force_brute_list() :**
+
+* Appel à enum() : $O(2^{n})$
+* Calcul du coût de chaque séquence de déplacements : $O(n)$
+* Renvoi le minimum d'une liste : $O(n)$ 
+
+force_brute_list() est donc un $O(2^{n})$.
+	
+Côté mémoire, enum() crée $2^{n}$ listes de taille n, n étant la longueur de 
+la séquence de requêtes.  
+Dans costs sont donc stockés $2^{n}$ nombres.  
+L'espace mémoire maximal utilisé est alors de $n * 2^{n} + 2^{n}$.
+	
+**force_brute_array() :**
+
+* Calcul de la taille de req : $O(n)$
+* Appel à range() : $O(2^{n})$
+* n appels à binaire() : $O(n^{2})$
+* n appels à cout_array() : $O(n)$
+
+force_brute_array() est donc un $O(2^{n})$.
+	
+Côté mémoire, on stocke un nombre dans len, une liste de taille $2^{n}$ dans r
+et un nombre dans m.  
+L'espace mémoire maximal utilisé est alors de $2^{n}$.
+
+En effectuant $10^{9}$ opérations à la seconde, on en fait $10^{6}$ en
+une milliseconde. Alors $10^{6} = 2^{n}$.  
+On peut donc espérer traiter une séquence de requêtes d'une taille de l'ordre 
+de $\frac{\ln{10^{6}}}{\ln{2}} = 20$.
 *)
 
 (* IV. *)
 (* IV.1. *)
 (*
-	On aura nécessairement une tête en position r_{n}. L'autre peut alors prendre 
+	On aura nécessairement une tête en position $r_{n}$. L'autre peut alors prendre 
 	toutes les autres places possibles. L'emsemble des paires de positions est donc :
 	 
-  \lbrace \lbrace 0, r_{n} \rbrace \rbrace \cup \lbrace \lbrace r_{k}, r_{n} \rbrace, k \in ℕ, 1 \leq k \leq n-1 \rbrace
+  $\lbrace \lbrace 0, r_{n} \rbrace \rbrace \cup \lbrace \lbrace r_{k}, r_{n} \rbrace \mid k \in ℕ \mid 1 \leq k \leq n-1 \rbrace$
 *)
 
 (* IV.2.a. *)
 (*
-  On obtient :
-  
-  $t_{3} = (t_{3, 0}, t_{3, 1}, t_{3, 2}) = (23, 21, 20)$
+	On obtient : $t_{3} = (t_{3, 0}, t_{3, 1}, t_{3, 2}) = (23, 21, 20)$
 *)
 
 (* IV.2.b. *)
@@ -393,7 +400,7 @@ let force_brute_array req =
   Donc $t_{k+1,i}$ = $t_{k,i} + |r_{k+1} - r_{k}|$.
   
   * Si $i = k$ :  
-  TODO
+  
 *)
 
 (* IV.2.d. *)
@@ -415,7 +422,7 @@ let update tk req k =
 
 (* IV.2.e. *)
 (*
-  update() est clairement un O(k).
+  update() est clairement un $O(k)$.
 *)
 
 (* IV.3. *)
@@ -440,18 +447,18 @@ let cout_opt req =
 
 (* IV.4. *)
 (*
-  - appel à vect_length : O(n)
-  - environ n appels à update() : O($\sum_{k=1}^{n-1} (k)$) = O(n*n)
-  - min_array : O(n)
-  cout_opt() est donc un O(n*n).
-  
-  En effectuant 10^{9} opérations à la seconde, on en fait 10^{6} en
-  une milliseconde. 
-  On peut donc espérer traiter une séquence de requêtes d'une taille de l'ordre 
-  de $ \sqrt[2]{10^{6}} = 1000$. 
+**Complexité :**  
+
+* vect_length() : $O(n) $ 
+* Environ n appels à update() : $O(\sum_{k=1}^{n-1} k) = O(n^{2})$  
+* min_array() : $O(n)$  
+
+cout_opt() est donc un $O(n^{2})$.
+
+En effectuant $10^{9}$ opérations à la seconde, on en fait $10^{6}$ en
+une milliseconde. Alors $10^{6} = n^{2}$.  
+On peut donc espérer traiter une séquence de requêtes d'une taille de l'ordre 
+de $\sqrt{10^{6}} = 1000$. 
 *)
 
 (* V. *)
-(*
-  TODO
-*)
